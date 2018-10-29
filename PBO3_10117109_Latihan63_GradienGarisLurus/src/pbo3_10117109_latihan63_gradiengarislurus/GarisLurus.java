76bee78325eb4977ae9ee99c1a290546311e2823
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-10-29 23:01:02 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan63_GradienGarisLurus/src/pbo3_10117109_latihan63_gradiengarislurus/GarisLurus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan63_gradiengarislurus;
+
+/**
+ * Nama  : Stefanus Saputra
+ * NIM   : 10117109
+ * Kelas : IF 3
+ * Deskripsi Program : program ini berisi program untuk menampilkan hasil dari 
+ *                     hitung gradien dengan x dan y yg sudah ditentukan 
+ *                     berdasarkan konsep interface
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
